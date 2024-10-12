1685c01647a53ae057266225470b0ffc99e4f634
Nathawat Wai <nathawat12246@gmail.com>
2024-10-12 23:03:36 +0700

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
 'use client';
 
-import  {Button}  from '@/components/ui/button';
-import {Money} from '@/components/elements/money';
-
+import { Button } from '@/components/ui/button';
+import { Money } from '@/components/elements/money';
 
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
       <Button>Click me</Button>
-      <Money/>
-      <div className='font-serif'>
-        ภาษาไทย English
-      </div>
-      <div className='font-BaiJamjuree'>
-        ภาษาไทย English
-      </div>
-      <div className='font-Anuphan'>
-        ภาษาไทย English
-      </div>
+      <Money />
     </div>
   );
 }
--- src/components/elements/money.tsx
+++ src/components/elements/money.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
 import {
   Dialog,
-  DialogClose,
+  DialogTitle,
   DialogContent,
   DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
+  DialogDescription,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 const Money = () => {
-  const [expectedBudget, setExpectedBudget] = useState(0);
-  const [realBudget, setRealBudget] = useState(0);
-  const [usedBudget, setUsedBudget] = useState(0);
+  const [expectedBudget, setExpectedBudget] = useState<number>();
+  const [realBudget, setRealBudget] = useState<number>();
+  const [usedBudget, setUsedBudget] = useState<number>();
   const [isOpen, setIsOpen] = useState(false); // Manage dialog open state
 
   const [tempExpectedBudget, setTempExpectedBudget] = useState(expectedBudget);
@@ -33,7 +33,6 @@ const Money = () => {
     setUsedBudget(tempUsedBudget);
 
     setIsOpen(false);
-
   };
 
   const handleSubmit = () => {
@@ -42,11 +41,11 @@ const Money = () => {
     setTempRealBudget(realBudget);
     setTempUsedBudget(usedBudget);
 
-    console.log(
-      "expectedBudget:"+expectedBudget+
-      "\nrealBudget:"+realBudget+
-      "\nusedBudget:"+usedBudget
-    );
+    console.log({
+      expectedBudget: expectedBudget,
+      realBudget: realBudget,
+      usedBudget: usedBudget,
+    });
 
     // Optionally save data to your state or send it to a server
 
@@ -66,71 +65,67 @@ const Money = () => {
         </div>
       </DialogTrigger>
       <DialogContent className="w-[360px] h-72 p-3 bg-white rounded-md border border-brown flex-col justify-start items-start gap-4 inline-flex">
-        <div className="self-stretch h-[200px] px-1.5 pt-9 flex-col justify-start items-start gap-4 flex">
+        <DialogTitle className="self-stretch h-[200px] px-1.5 pt-9 flex-col justify-start items-start gap-4 flex">
+          <DialogDescription className="hidden" />
           <div className="self-stretch justify-start items-center gap-4 inline-flex">
             <Label
               htmlFor="name"
-              className="w-20 text-black text-sm font-normal font-Anuphan leading-normal"
-            >
+              className="w-20 text-black text-sm font-normal font-Anuphan leading-normal">
               งบประมาณ:
             </Label>
             <Input
               id="expectedBudget"
               value={expectedBudget}
-              onChange={(e) => setExpectedBudget(parseInt(e.target.value))}
-              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-brown underline"
+              onChange={(e) => setExpectedBudget(Number.parseFloat(e.target.value))}
+              type="number"
+              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-brown "
               placeholder="Add Budget..."
             />
           </div>
           <div className="self-stretch justify-start items-center gap-4 inline-flex">
             <Label
               htmlFor="name"
-              className="w-20 text-black text-sm font-normal font-['Inter'] leading-normal"
-            >
+              className="w-20 text-black text-sm font-normal font-['Inter'] leading-normal">
               สำรองจ่าย:
             </Label>
             <Input
               id="realBudget"
               value={realBudget}
-              onChange={(e) => setRealBudget(parseInt(e.target.value))}
-              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-green underline"
+              onChange={(e) => setRealBudget(Number.parseFloat(e.target.value))}
+              type="number"
+              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-green "
               placeholder="Add Budget..."
             />
           </div>
           <div className="self-stretch justify-start items-center gap-4 inline-flex ">
-            <Label
-              htmlFor="name"
-              className="w-20 text-black text-sm font-normal  leading-normal"
-            >
+            <Label htmlFor="name" className="w-20 text-black text-sm font-normal  leading-normal">
               เบิกจริง:
             </Label>
             <Input
               id="usedBudget"
               value={usedBudget}
-              onChange={(e) => setUsedBudget(parseInt(e.target.value))}
-              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-red underline"
+              onChange={(e) => setUsedBudget(Number.parseFloat(e.target.value))}
+              type="number"
+              className="h-10 w-[240px] px-4 bg-white rounded-md border border-brown text-red "
               placeholder="Add Budget..."
             />
           </div>
-        </div>
+        </DialogTitle>
         <div className="self-stretch px-3 pt-3 border-t border-gray-300 justify-between items-start inline-flex">
           <Button
             onClick={handleClear}
-            className="h-10 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white"
-          >
+            className="h-10 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white">
             Clear
           </Button>
           <div className="grow shrink basis-0 h-10 justify-end items-start gap-2 flex">
             <Button
               onClick={handleCancel}
-              className="h-10 w-20 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white hover:bg-neutral-900/90"
-            >
+              className="h-10 w-20 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white hover:bg-neutral-900/90">
               Cancel
             </Button>
             <Button
               onClick={handleSubmit}
-              className="h-10 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white"
-            >
+              className="h-10 bg-inherit rounded-[100px] flex-col justify-center items-center gap-2 inline-flex text-brown text-sm font-normal font-BaiJamjuree  hover:text-white">
               Ok
             </Button>
           </div>
