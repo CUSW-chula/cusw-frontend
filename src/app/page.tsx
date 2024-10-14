9324c373024e5965aee41acb29cd19755a049134
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-14 22:09:02 +0700

    feat(money.tsx): finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Money } from '@/components/elements/money';
 
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
       <Button>Click me</Button>
-      <Money />
     </div>
   );
 }
