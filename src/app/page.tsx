939658772fb8f26101e1e6065ce70ca34251d200
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-16 01:47:30 +0700

    test(page.tsx): test deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import CounterGrid from '@/components/examples/example';
-import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
-      <Button>Click me</Button>
       <CounterGrid />
       <CounterGrid />
       <CounterGrid />
