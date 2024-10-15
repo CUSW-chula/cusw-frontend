a46b25c15c60e425fe5069ffb2ebc1886e75fc0e
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-16 02:05:31 +0700

    test: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import CounterGrid from '@/components/examples/example';
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
-      <div>deploy succces</div>
       <CounterGrid />
       <CounterGrid />
       <CounterGrid />
