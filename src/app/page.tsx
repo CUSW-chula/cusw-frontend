68dbb6fb1915fccadd8c54182e312dedc784a3b6
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-16 01:44:38 +0700

    fix(page.tsx): minor change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
-      <Button>Click mee</Button>
+      <Button>Click me</Button>
       <CounterGrid />
       <CounterGrid />
       <CounterGrid />
