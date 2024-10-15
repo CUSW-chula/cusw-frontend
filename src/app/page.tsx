f8d98599b38e8e97bd68362d6ff5cba9b6f0f3f8
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-16 00:43:44 +0700

    build(deploy.yml): set github action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
-      <Button>Click me</Button>
+      <Button>Click mee</Button>
       <CounterGrid />
       <CounterGrid />
       <CounterGrid />
