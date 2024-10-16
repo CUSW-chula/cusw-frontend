822f92733926a7b93a8a3adbc1c25485ccd33b2f
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-10-17 03:45:55 +0700

    fix(ecosystem.js): fix run dev env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,17 +3,20 @@ module.exports = {
     {
       name: 'cusw',
       exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
+      instances: 'max', // Or a specific number of instances
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
       env_local: {
-        APP_ENV: 'local' // APP_ENV=local
+        APP_ENV: 'local', // APP_ENV=local
+        PORT: 3000 // Run on port 3000 in local
       },
       env_dev: {
-        APP_ENV: 'dev' // APP_ENV=dev
+        APP_ENV: 'dev', // APP_ENV=dev
+        PORT: 4000 // Run on port 4000 in development
       },
       env_prod: {
-        APP_ENV: 'prod' // APP_ENV=prod
+        APP_ENV: 'prod', // APP_ENV=prod
+        PORT: 3000 // Default port in production
       }
     }
   ]
