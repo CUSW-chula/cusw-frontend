a9f558e5c11ad480b3dccefa4b3c6aed6235088d
JirayuNam <6534410523@student.chula.ac.th>
2024-10-17 14:56:00 +0700

    feat(date-feature): add date-feature component in element

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { DatePickerWithRange } from '@/components/elements/date-feature';
 
 export default function Home() {
   return (
     <div className="min-w-full min-h-full flex flex-col items-center justify-center mt-10 gap-10">
-      <Button>Click me</Button>
+      <Button>Click me</Button>s
     </div>
   );
 }
