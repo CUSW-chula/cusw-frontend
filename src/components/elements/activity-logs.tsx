c20443211cd9d0cadf8ee1a2b20a6a758f572ff7
Mintada P. <mintada.phuangminthada@gmail.com>
2024-10-23 04:21:05 +0700

    style(feat(activity-logs)): make log display only icon and add hover func to profile icon

--- src/components/elements/activity-logs.tsx
+++ src/components/elements/activity-logs.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui/tooltip';
 import type React from 'react';
 
 // Dummy data for activity logs
@@ -58,11 +59,18 @@ function formatDate(date?: Date): string {
 function ActivityLogItem({ user, action, detail, createdAt }: ActivityLogItemProps) {
   return (
     <div className="flex items-center space-x-2 text-base text-gray-800 font-BaiJamjuree">
-      {/* Circle with initials */}
-      <div className="w-10 h-10 bg-slate-200 rounded-full flex items-center justify-center">
-        <span className="text-slate-900">{getInitials(user)}</span>
-      </div>
-      <span className="font-semibold">{user}</span>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <div className="w-10 h-10 bg-slate-200 rounded-full flex items-center justify-center">
+              <span className="text-slate-900">{getInitials(user)}</span>
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>
+            <span>{user}</span>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <span>{action}</span>
       <span className="font-semibold">{BoldAfterToFrom(detail)}</span>
       <span className="text-gray-500 pl-1 text-sm">{formatDate(createdAt)}</span>
@@ -73,22 +81,22 @@ function ActivityLogItem({ user, action, detail, createdAt }: ActivityLogItemPro
 // Main ActivityLogs component
 const ActivityLogs: React.FC = () => {
   return (
-    <div className="bg-white space-y-2">
-      <h3 className="text-2xl font-semibold text-gray-900 mb-4 font-Anuphan">Activity</h3>
-      <ul>
-        {activityLogs.map((item) => (
-          <li key={item.id}>
-            <ActivityLogItem
-              user={item.user}
-              action={item.action}
-              detail={item.detail}
-              createdAt={item.createdAt}
-              id={item.id}
-            />
-          </li>
-        ))}
-      </ul>
-    </div>
+      <div className="bg-white space-y-2">
+        <h3 className="text-2xl font-semibold text-gray-900 mb-4 font-Anuphan">Activity</h3>
+        <ul>
+          {activityLogs.map((item) => (
+            <li key={item.id}>
+                <ActivityLogItem
+                  user={item.user}
+                  action={item.action}
+                  detail={item.detail}
+                  createdAt={item.createdAt}
+                  id={item.id}
+                />
+            </li>
+          ))}
+        </ul>
+      </div>
   );
 };
 
