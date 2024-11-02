42ac7cd407f58d6bd8fe56603a9e3de065a58260
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-11-03 03:24:05 +0700

    fix: typo

--- src/app/tasks/[task_id]/page.tsx
+++ src/app/tasks/[task_id]/page.tsx
@@ -16,15 +16,15 @@ export default function TasksManageMentPage({ params }: TaskManageMentProp) {
     <div className="min-w-full min-h-screen flex flex-col lg:flex-row items-start justify-center mt-10 gap-8">
       {/* Left Section */}
       <div className="w-full lg:w-[60%] rounded-[6px] p-5 border-brown border-[1px] bg-white">
-        <Workspace task_id={task_id?.toString() ?? ''} />
+        <Workspace task_id={task_id} />
         <div className="flex flex-col gap-4 mt-4">
           <ActivityLogs />
-          <Comment task_id={task_id?.toString() ?? ''} />
+          <Comment task_id={task_id} />
         </div>
       </div>
 
       {/* Right Section */}
-      <MenuBar task_id={task_id?.toString() ?? ''} />
+      <MenuBar task_id={task_id} />
     </div>
   );
 }
