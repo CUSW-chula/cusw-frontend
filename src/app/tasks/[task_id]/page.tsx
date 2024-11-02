24365571c112a4e9f67717c904decc5c535b2ab2
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-11-03 03:18:22 +0700

    feat(task.tsx): make import ssr true

--- src/app/tasks/[task_id]/page.tsx
+++ src/app/tasks/[task_id]/page.tsx
@@ -1,15 +1,17 @@
-'use client';
-
 import dynamic from 'next/dynamic';
 import ActivityLogs from '@/components/elements/activity-logs';
 import Comment from '@/components/elements/comment';
 import { MenuBar } from '@/components/elements/menu-bar';
-import { useParams } from 'next/navigation';
 
-export default function TasksManageMentPage() {
-  const Workspace = dynamic(() => import('../../../components/elements/workspace'), { ssr: false });
-  const { task_id } = useParams();
+interface TaskManageMentProp {
+  params: {
+    task_id: string;
+  };
+}
 
+export default function TasksManageMentPage({ params }: TaskManageMentProp) {
+  const Workspace = dynamic(() => import('../../../components/elements/workspace'), { ssr: true });
+  const { task_id } = params;
   return (
     <div className="min-w-full min-h-screen flex flex-col lg:flex-row items-start justify-center mt-10 gap-8">
       {/* Left Section */}
