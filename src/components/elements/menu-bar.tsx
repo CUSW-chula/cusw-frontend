bf16ad14c54cc434f23f76caa785317ff9e82edc
Siwadol_23 <siwadolrungmart@gmail.com>
2024-11-15 01:43:14 +0700

    fix(fix newtag and fix menu bar): fix newTAg and fix menu bar

--- src/components/elements/menu-bar.tsx
+++ src/components/elements/menu-bar.tsx
@@ -6,14 +6,17 @@ import StatusButton from './status-botton';
 import StatusLabel from './status-label';
 import { ProjectOwner } from './project-owner';
 import { AssignedTaskToMember } from './assigned-task';
-import { ButtonAddTags } from './button-add-tag';
-import type { TaskManageMentProp } from '@/lib/shared';
-import { DatePickerWithRange } from './date-feature';
+import { ButtonAddTags } from "./button-add-tag";
+import type { TaskManageMentProp } from "@/lib/shared";
+import { DatePickerWithRange } from "./date-feature";
 
 const MenuBar = ({ task_id }: TaskManageMentProp) => {
   return (
-    <div className="h-[400px] w-[395px] p-5 bg-white rounded-md border border-[#6b5c56] flex-col justify-center items-start gap-4 inline-flex">
-      <div aria-label="status" className="h-10 justify-start items-center inline-flex">
+    <div className="min-h-[400px] w-[395px] p-5 bg-white rounded-md border border-[#6b5c56] flex-col justify-center items-start gap-4 inline-flex">
+      <div
+        aria-label="status"
+        className="h-10 justify-start items-center inline-flex"
+      >
         {/* Label Zone */}
         <div className="w-24 justify-start items-center gap-2 flex">
           {/* Describtion */}
@@ -24,43 +27,58 @@ const MenuBar = ({ task_id }: TaskManageMentProp) => {
         {/* Content */}
         <StatusButton task_id={task_id} />
       </div>
-      <div aria-label="owner" className="h-10 justify-start items-center inline-flex">
+      <div
+        aria-label="owner"
+        className="h-10 justify-start items-center inline-flex"
+      >
         {/* Label Zone */}
         <div className="w-24 justify-start items-center gap-2 flex">
           {/* Icon */}
           <CrownIcon className="w-6 h-6 relative" />
           {/* Describtion */}
           <div className="text-[#6b5c56] text-xs font-medium font-['Bai Jamjuree'] leading-tight">
-            Owner :{' '}
+            Owner :{" "}
           </div>
         </div>
         <ProjectOwner />
       </div>
-      <div aria-label="member" className="h-10 justify-start items-center inline-flex">
+      <div
+        aria-label="member"
+        className="h-10 justify-start items-center inline-flex"
+      >
         {/* Label Zone */}
         <div className="w-24 justify-start items-center gap-2 flex">
           {/* Icon */}
           <Users className="w-6 h-6 relative" />
           {/* Describtion */}
           <div className="text-[#6b5c56] text-xs font-medium font-['Bai Jamjuree'] leading-tight">
-            Member :{' '}
+            Member :{" "}
           </div>
         </div>
         <AssignedTaskToMember task_id={task_id} />
       </div>
-      <div aria-label="tag" className="h-10 justify-start items-center inline-flex">
+      <div
+        aria-label="tag"
+        className="justify-start items-center inline-flex flex-wrap w-full"
+      >
         {/* Label Zone */}
-        <div className="w-24 justify-start items-center gap-2 flex">
+        <div className="w-24 justify-start items-center gap-2 flex self-start ">
           {/* Icon */}
           <Tag className="w-6 h-6 relative" />
-          {/* Describtion */}
+          {/* Description */}
           <div className="text-[#6b5c56] text-xs font-medium font-['Bai Jamjuree'] leading-tight">
-            Tag :{' '}
+            Tag :{" "}
           </div>
-        </div>
-        <ButtonAddTags task_id={task_id} />
+      </div >
+      <div className='flex w-[253.67px] '>
+      <ButtonAddTags task_id={task_id} />
       </div>
-      <div aria-label="money" className="h-10 justify-start items-center inline-flex">
+        
+      </div>
+      <div
+        aria-label="money"
+        className="h-10 justify-start items-center inline-flex"
+      >
         {/* Label Zone */}
         <div className="w-24 justify-start items-center gap-2 flex">
           {/* Icon */}
@@ -70,24 +88,26 @@ const MenuBar = ({ task_id }: TaskManageMentProp) => {
 
           {/* Describtion */}
           <div className="text-[#6b5c56] text-xs font-medium font-['Bai Jamjuree'] leading-tight">
-            Money :{' '}
+            Money :{" "}
           </div>
         </div>
         <Money />
       </div>
-      <div aria-label="date" className="h-10 justify-start items-center inline-flex">
+      <div
+        aria-label="date"
+        className="h-10 justify-start items-center inline-flex"
+      >
         {/* Label Zone */}
         <div className="w-24 justify-start items-center gap-2 flex">
           {/* Icon */}
           <Calendar className="w-6 h-6 relative" />
           {/* Describtion */}
           <div className="text-[#6b5c56] text-xs font-medium font-['Bai Jamjuree'] leading-tight">
-            Date :{' '}
+            Date :{" "}
           </div>
         </div>
         <DatePickerWithRange />
       </div>
-      <Trash2 className="h-6 w-6" />
     </div>
   );
 };
