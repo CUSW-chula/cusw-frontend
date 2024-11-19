6119b86af60f2a0efee44ed89c0715e6cfcc4819
Mintada P. <mintada.phuangminthada@gmail.com>
2024-11-19 19:11:41 +0700

    style: add projec title to breadcrumb

--- src/components/elements/breadcrumb.tsx
+++ src/components/elements/breadcrumb.tsx
@@ -5,42 +5,30 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbList,
-  BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
 import BASE_URL from '@/lib/shared';
 import { getCookie } from 'cookies-next';
 import React from 'react';
 import { useEffect, useState } from 'react';
+import { ChevronRight } from 'lucide-react';
 
 interface BreadcrumbProps {
   task_id: string;
-}
-
-async function getTitleName(taskId: string, auth: string) {
-  try {
-    const response = await fetch(`${BASE_URL}/tasks/title/${taskId}`, {
-      headers: {
-        Authorization: auth,
-      },
-    });
-    if (!response.ok) {
-      throw new Error(`Error: ${response.statusText}`);
-    }
-    const data = await response.json();
-    return data.title;
-  } catch (error) {
-    console.error('Failed to fetch task title:', error);
-    return 'Unknown';
-  }
 }
 
 export function BreadcrumbComponent({ task_id }: BreadcrumbProps) {
   interface Task {
     id: string;
     title: string;
+    project_id: string;
+  }
+  interface Project {
+    id: string;
+    title: string;
   }
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [project, setProject] = useState<Project | null>(null);
   const cookie = getCookie('auth');
   const auth = cookie?.toString() ?? '';
 
@@ -57,20 +45,41 @@ export function BreadcrumbComponent({ task_id }: BreadcrumbProps) {
         }
         const data = await response.json();
         setTasks(data);
+
+        if (data.length > 0) {
+          const projectId = data[0].projectId;
+          const projectResponse = await fetch(`${BASE_URL}/projects/${projectId}`, {
+            headers: {
+              Authorization: auth,
+            },
+          });
+          if (!projectResponse.ok) {
+            throw new Error(`Error: ${projectResponse.statusText}`);
+          }
+          const projectData = await projectResponse.json();
+          setProject(projectData);
+        }
       } catch (error) {
-        console.error('Failed to fetch task titles:', error);
+        console.error('Failed to fetch data:', error);
       }
     };
-    getTitleName(task_id, auth);
     fetchData();
   }, [task_id, auth]);
 
   return (
     <div>
-      <Breadcrumb>
-        <BreadcrumbList>
-          {tasks.map((task, index) => (
+      <Breadcrumb className="flex flex-row">
+        <BreadcrumbList className="gap-6">
+          <BreadcrumbItem>
+            <BreadcrumbLink
+              href={`/projects/${project?.id}`}
+              className="font-BaiJamjuree text-lg text-black">
+              {project?.title}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          {tasks.map((task) => (
             <React.Fragment key={task.id}>
+              {<BreadcrumbSeparator />}
               <BreadcrumbItem>
                 <BreadcrumbLink
                   href={`/tasks/${task.id}`}
@@ -78,7 +87,6 @@ export function BreadcrumbComponent({ task_id }: BreadcrumbProps) {
                   {task.title}
                 </BreadcrumbLink>
               </BreadcrumbItem>
-              {index < tasks.length - 1 && <BreadcrumbSeparator />}
             </React.Fragment>
           ))}
         </BreadcrumbList>
