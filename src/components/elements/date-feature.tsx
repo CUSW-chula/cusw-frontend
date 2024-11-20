de0fd5156c181b6887d60c464626b09c15f8a893
JirayuNam <6534410523@student.chula.ac.th>
2024-11-20 20:26:48 +0700

    feat: connect to date-feature to backend, connect websocket to real time co-working
    BREAKING CHANGE: When using DatePickerWithRange have to pass task_id also
    
    Date feature backend connection #51

--- src/components/elements/date-feature.tsx
+++ src/components/elements/date-feature.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import { format, parse, isValid, addDays } from 'date-fns';
+import { format, parse, isValid, addDays, set, formatDate } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
 import type { DateRange } from 'react-day-picker';
 
@@ -21,13 +21,15 @@ interface DateInterface {
 function DatePickerWithRange({ task_id }: TaskManageMentProp) {
   const [date, setDate] = React.useState<DateRange | undefined>({
     from: new Date(),
+    to: new Date(),
   });
   const [inputValue, setInputValue] = React.useState<string>('');
   const cookie = getCookie('auth');
   const auth = cookie?.toString() ?? '';
 
   // Regex for date matching
-  const dateTimeRegEx = /^(\d{1,2})\/(\d{1,2})\/(\d{2,4})$/;
+  const dateTimeRegEx = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+  const dateTimeRegEx2 = /^(\d{4})-(\d{2})-(\d{2})/;
 
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   const parseJsonValue = React.useCallback((values: any) => {
@@ -42,12 +44,15 @@ function DatePickerWithRange({ task_id }: TaskManageMentProp) {
   React.useEffect(() => {
     // Fetch Data
     const fetchDate = async () => {
-      const curDate = await fetch(`${BASE_URL}/tasks/getdate/${task_id}`, {
+      const curDate = await fetch(`${BASE_URL}/tasks/date/${task_id}`, {
         headers: {
           Authorization: auth,
         },
       });
-      console.log(curDate);
+      const initDate = await curDate.json();
+      const from = initDate[0];
+      const to = initDate[1];
+      setDate({ from, to });
     };
 
     fetchDate();
@@ -62,17 +67,18 @@ function DatePickerWithRange({ task_id }: TaskManageMentProp) {
       console.log('Message received:', event.data);
 
       try {
-        const url = `${BASE_URL}/tasks/date`;
-        const options = {
-          method: 'PATCH',
-          headers: { 'Content-Type': 'application/json', Authorization: auth },
-          body: JSON.stringify({ taskId: task_id, startDate: date?.from, endDate: date?.to }),
-        };
-
-        try {
-          await fetch(url, options);
-        } catch (error) {
-          console.error(error);
+        // Connect web socket
+        const socketEvent = JSON.parse(event.data);
+        const eventName = socketEvent.eventName;
+        const data = parseJsonValue(socketEvent.data);
+        if (eventName === 'date') {
+          const start = data.startDate?.toString();
+          const end = data.endDate?.toString();
+          console.log('start and type:', start, typeof start);
+          console.log('end and type:', end, typeof end);
+          const from = start ? new Date(start) : undefined;
+          const to = end ? new Date(end) : undefined;
+          setDate({ from, to });
         }
       } catch (error) {
         console.error('Error parsing WebSocket message:', error);
@@ -86,23 +92,64 @@ function DatePickerWithRange({ task_id }: TaskManageMentProp) {
     return () => {
       ws.close();
     };
-  }, [date, task_id, auth]);
+  }, [parseJsonValue, task_id, auth]);
+
+  // useEffect to handle inputValue when date change
+  React.useEffect(() => {
+    if (date?.from) {
+      setInputValue(
+        date.to
+          ? `${format(date.from, 'dd/MM/yyyy')} - ${format(date.to, 'dd/MM/yyyy')}`
+          : inputValue.length > format(date.from, 'dd/MM/yyyy').length
+            ? inputValue
+            : format(date.from, 'dd/MM/yyyy'),
+      );
+    } else {
+      setInputValue(inputValue);
+    }
+  }, [date, inputValue]);
 
   // Handle input changes and update calendar state based on the input
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setInputValue(value);
+    // console.log('input value:', value)
+    await setInputValue(value);
 
     // Split from and to date
-    const [fromStr, toStr] = value.split(' - ').map((dateStr) => dateStr.trim());
+    const [fromStr, toStr] = await value.split('- ').map((dateStr) => dateStr.trim());
 
     // Parse date from string
     const from =
-      fromStr && dateTimeRegEx.test(fromStr) ? parse(fromStr, 'dd/MM/yyyy', new Date()) : undefined;
+      (await fromStr) && dateTimeRegEx.test(fromStr)
+        ? parse(fromStr, 'dd/MM/yyyy', new Date())
+        : undefined;
     const to =
-      toStr && dateTimeRegEx.test(toStr) ? parse(toStr, 'dd/MM/yyyy', new Date()) : undefined;
+      (await toStr) && dateTimeRegEx.test(toStr)
+        ? parse(toStr, 'dd/MM/yyyy', new Date())
+        : undefined;
 
-    // Check state in varaible: console.log(value + "\n" + fromStr + " " + toStr + "\n" + from + " " + to);
+    // Check state in varaible:
+    // console.log(value + "\n" + fromStr + " " + toStr + "\n" + from + " " + to + "\n");
+    // console.log('Cur input value:', inputValue);
+
+    const url = `${BASE_URL}/tasks/date`;
+    const options = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json', Authorization: auth },
+      // body: `{"taskID":"${task_id}", "startDate":"${range?.from}", "endDate":"${range?.to}"}`
+      body: JSON.stringify({
+        taskID: task_id,
+        startDate: from ? from : null,
+        endDate: to ? to : null,
+      }),
+    };
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      // console.log(data); // Check fetch data
+    } catch (error) {
+      console.error(error);
+    }
 
     if (isValid(from) && isValid(to)) {
       setDate({ from, to });
@@ -112,18 +159,32 @@ function DatePickerWithRange({ task_id }: TaskManageMentProp) {
   };
 
   // Handle calendar selection
-  const handleCalendarSelect = (range: DateRange | undefined) => {
-    setDate(range);
-
-    if (range?.from) {
-      setInputValue(
-        range.to
-          ? `${format(range.from, 'MM/dd/yyyy')} - ${format(range.to, 'MM/dd/yyyy')}`
-          : format(range.from, 'MM/dd/yyyy'),
-      );
-    } else {
-      setInputValue('');
+  const handleCalendarSelect = async (range: DateRange | undefined) => {
+    // Patch input to database.
+    const url = `${BASE_URL}/tasks/date`;
+    const options = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json', Authorization: auth },
+      // body: `{"taskID":"${task_id}", "startDate":"${range?.from}", "endDate":"${range?.to}"}`
+      body: JSON.stringify({
+        taskID: task_id,
+        startDate: range?.from ? range.from : null,
+        endDate: range?.to ? range.to : null,
+      }),
+    };
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      if (data) {
+        setDate(range);
+      }
+      // console.log(data); // Check Fetch Data
+    } catch (error) {
+      console.error(error);
     }
+
+    //Show data in range
+    console.log('range from selected date:', range);
   };
 
   return (
@@ -135,7 +196,7 @@ function DatePickerWithRange({ task_id }: TaskManageMentProp) {
               value={inputValue}
               onChange={handleInputChange}
               placeholder="DD/MM/YYYY - DD/MM/YYYY"
-              className="w-[300px] pl-10" // Add padding for the icon
+              className="w-auto pl-10" // Add padding for the icon
             />
             <CalendarIcon className="absolute left-3 top-3 h-5 w-5 text-gray-400 pointer-events-none" />
           </div>
