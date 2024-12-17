a7af6990ee98fd5be4abb1f9b49f31c58aa3692a
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-12-18 01:39:46 +0700

    feat(blocknote): get username

--- src/components/elements/blocknote.tsx
+++ src/components/elements/blocknote.tsx
@@ -17,18 +17,23 @@ import * as Tooltip from '@/components/ui/tooltip';
 import { useEffect, useState } from 'react';
 import BASE_URL, { type TaskManageMentProp } from '@/lib/shared';
 import { getCookie } from 'cookies-next';
+import { jwtDecode, type JwtPayload } from 'jwt-decode';
+import { describe } from 'node:test';
+
 const cookie = getCookie('auth');
 const auth = cookie?.toString() ?? '';
-
+interface CustomJwtPayload extends JwtPayload {
+  id: string;
+}
 export default function Blocknotes({ task_id }: TaskManageMentProp) {
   const docId = task_id;
-
   return (
     <YDocProvider docId={docId} authEndpoint="https://demos.y-sweet.dev/api/auth">
       <Document task_id={task_id} />
     </YDocProvider>
   );
 }
+
 function getRandomLightColor(): string {
   const getLightValue = () => Math.floor(Math.random() * 128) + 128;
   const r = getLightValue().toString(16).padStart(2, '0');
@@ -91,15 +96,44 @@ function Document({ task_id }: TaskManageMentProp) {
       ...allowedBlockSpecs,
     },
   });
+
+  const getUserDataFromCookie = () => {
+    const decoded = jwtDecode<CustomJwtPayload>(auth);
+    return decoded;
+  };
+
+  const userData = getUserDataFromCookie();
+  const [name, setName] = useState<string>('Guest');
+
+  useEffect(() => {
+    async function fetchUserName() {
+      try {
+        const response = await fetch(`${BASE_URL}/users/${userData.id}`, {
+          headers: {
+            Authorization: auth,
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setName(data.name);
+        } else {
+          console.error('Error fetching user name');
+        }
+      } catch (error) {
+        console.error('Error fetching user name:', error);
+      }
+    }
+    fetchUserName();
+  });
+
   const provider = useYjsProvider();
   const doc = useYDoc();
-
   const editor = useCreateBlockNote({
     schema,
     collaboration: {
       provider,
       fragment: doc.getXmlFragment('blocknote'),
-      user: { color: getRandomLightColor(), name: 'userData' },
+      user: { color: getRandomLightColor(), name: 'name' },
     },
   });
 
