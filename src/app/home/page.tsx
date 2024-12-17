6111f9d3fe606cc5b8e2b40a1a4a8d37f9aca532
banyaphon <92526947+bypkt-bk@users.noreply.github.com>
2024-12-17 22:21:29 +0700

    feat(search): search bar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import Search from '@/components/elements/search';
+
+export default function Home() {
+  return (
+    <div className="relative">
+      <Search />
+    </div>
+  );
+}
---
+++ src/components/elements/search.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+const Search = () => {
+  return (
+    <Command>
+      <CommandInput placeholder="search the project..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading="Suggestions">
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
+};
+
+export default Search;
