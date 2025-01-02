f870502b572fec48253f9c42f3dea045f7de9c7d
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-01-02 20:29:19 +0700

    fix(versioning api): change base url api to v1

--- src/lib/shared.ts
+++ src/lib/shared.ts
@@ -29,7 +29,7 @@ export const BASE_SOCKET =
 
 const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://cusw-workspace.sa.chula.ac.th/api'
-    : 'http://localhost:4000/api';
+    ? 'https://cusw-workspace.sa.chula.ac.th/v1/api'
+    : 'http://localhost:4000/v1/api';
 
 export default BASE_URL;
