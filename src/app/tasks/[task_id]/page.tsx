ad24df43cc4b40c5e26892d8b9ecf360d8c64734
Siwadol_23 <siwadolrungmart@gmail.com>
2025-01-15 02:23:43 +0700

    feat(used api v2): used api v2

--- src/app/tasks/[task_id]/page.tsx
+++ src/app/tasks/[task_id]/page.tsx
@@ -16,6 +16,7 @@ interface TaskManageMentProp {
 export default async function TasksManageMentPage({ params }: TaskManageMentProp) {
   const Workspace = dynamic(() => import('../../../components/elements/workspace'), { ssr: true });
   const { task_id } = await params;
+  
   return (
     <div className="min-w-full min-h-screen flex-col items-start justify-center gap-8 ">
       {/* page nav */}
--- src/components/elements/projectList2.tsx
+++ src/components/elements/projectList2.tsx
@@ -1,47 +1,86 @@
-'use client';
-import { PopoverContent } from '@radix-ui/react-popover';
-import { CommandGroup, CommandItem } from 'cmdk';
-import { Calendar, CrownIcon, User, Users } from 'lucide-react';
-import * as React from 'react';
+"use client";
+import { PopoverContent } from "@radix-ui/react-popover";
+import { CommandGroup, CommandItem } from "cmdk";
+import {  Calendar, CrownIcon,  Users } from "lucide-react";
+import * as React from "react";
+import { Badge } from '@/components/ui/badge';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { getCookie } from "cookies-next";
+import BASE_URL from "@/lib/shared";
 
-import { Tooltip, TooltipContent, TooltipTrigger, TooltipProvider } from '@/components/ui/tooltip';
-import { getCookie } from 'cookies-next';
-import BASE_URL from '@/lib/shared';
-interface ProjectInterface {
+
+
+export type User = {
+  id: string;
+  email: string;
+  name: string;
+} | null;
+
+export type Tag = {
+  id: string;
+  name: string;
+} | null;
+
+export type Task = {
   id: string;
   title: string;
   description: string;
-  expectedBudget: number;
-  realBudget: number;
-  usedBudget: number;
-  startDate: Date;
-  endDate: Date;
-}
+  parentTaskId: string | null;
+  projectId: string;
+  startDate: Date | null;
+  endDate: Date | null;
+  expense: number;
+  createdById: string | null;
+  owner: User | null;
+  members: User[];
+  tags: Tag[];
+  budget: number;
+  advance: number;
+  // status: $Enums.TaskStatus;
+  subtasks: Task[];
+  emojis: Emoji[];
+};
 
-interface UsersInterfaces {
+export type Prorject = {
   id: string;
-  userName: string;
-}
-
-interface Date_st {
+  title: string;
+  description: string;
+  budget: number;
+  advance: number;
+  expense: number;
   startDate: Date;
   endDate: Date;
-}
-const item: Date_st = {
-  startDate: new Date(), // วันที่เริ่มต้น
-  endDate: new Date(), // วันที่สิ้นสุด
+  createdById: string;
+  owner: User[];
+  members: User[];
+  tasks: Task[];
+  tags: Tag[];
+};
+
+export type TaskAssignment = {
+  user: User;
+  task: Task;
+};
+
+export type Emoji = {
+  id: string;
+  emoji: string;
+  taskId: string;
+  user: User;
 };
 
 // mock data (default users)
-const users: UsersInterfaces[] = [
-  { id: '1', userName: 'Mintada Phuangminthada' },
-  { id: '2', userName: 'Dintada Ahuangminthada' },
-];
+
 
 export const ProjectList_2 = () => {
-  const cookie = getCookie('auth');
-  const auth = cookie?.toString() ?? '';
-  const [projectList, setProjectList] = React.useState<ProjectInterface[]>([]);
+  const cookie = getCookie("auth");
+  const auth = cookie?.toString() ?? "";
+  const [projectList, setProjectList] = React.useState<Prorject[]>([]);
 
   // ดึงข้อมูลโปรเจกต์เมื่อคอมโพเนนต์ถูกโหลด
   React.useEffect(() => {
@@ -53,86 +92,119 @@ export const ProjectList_2 = () => {
         const data = await response.json();
         setProjectList(data);
       } catch (error) {
-        console.error('Error fetching projects:', error);
+        console.error("Error fetching projects:", error);
       }
     };
     fetchProjectTitle();
   }, [auth]);
   //owner
-  const getInitials = (name: string) => {
-    const nameParts = name.split(' ');
-    return nameParts.map((part) => part[0]).join(''); // Take the first letter of each part
+  const getInitials = (name: string ) => {
+    const nameParts = name.split(" ");
+    return nameParts.map((part) => part[0]).join(""); // Take the first letter of each part
   };
 
   const getFirstName = (name: string) => {
-    const nameParts = name.split(' ');
+    const nameParts = name.split(" ");
     return nameParts[0];
   };
 
   const formatDate = (startdate: Date | null, enddate: Date | null): string => {
     // Return an empty string if both dates are not provided
-    if (!startdate || !enddate) return '';
+    if (!startdate || !enddate) return "";
 
     const format = (date: Date): string => {
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, "0");
+      const month = String(date.getMonth() + 1).padStart(2, "0");
       const year = date.getFullYear();
       return `${day}/${month}/${year}`;
     };
 
     // Format startdate and enddate if they are valid
-    const start = startdate ? format(startdate) : '';
-    const end = enddate ? format(enddate) : '';
+    const start = startdate ? format(startdate) : "";
+    const end = enddate ? format(enddate) : "";
 
-    return `${start}${start && end ? ' -> ' : ''}${end}`;
+    return `${start}${start && end ? " -> " : ""}${end}`;
   };
-
+  
   return (
     <>
-      <div className="flex items-start content-start gap-[16px] flex-wrap ">
+      <div className="flex items-start content-start gap-[16px] flex-wrap  ">
         {projectList.length > 0 ? (
           projectList.map((project) => (
             <div
               key={project.id}
-              className="flex flex-start w-[416px] h-[260px] p-[18px] gap-[10px] bg-white border-[1px] border-brown rounded-[6px]">
-              <div className="flex flex-start gap-[10px] rounded-[6px] self-stretch">
-                <img width={158} height={224} alt="img" src="/asset/Options.svg" />
-              </div>
+              className="flex flex-start w-[308px] h-[308px] p-[18px] gap-[10px] bg-white border-[1px] border-brown rounded-[6px] relative"
+            >
+              {/* <div className="flex flex-start gap-[10px] rounded-[6px] self-stretch">
+              </div> */}
               <div className="flex flex-col gap-y-[16px] ">
                 <div className="h-[56px] w-[204px] self-stretch">
                   <div className="font-BaiJamjuree text-[16px] text-base font-medium leading-[1.75] ">
                     {project.title}
                   </div>
                 </div>
+                <div className="absolute top-[60px] right-[20px] ">
+                <div className="flex flex-row flex-wrap items-center">
+                    {project.tags?.slice(0, 1).map((tag) => (
+                      <Badge
+                        key={tag?.id}
+                        variant="destructive"
+                        className="h-7 min-w-fit px-[8px] py-[12px] flex items-center justify-center bg-[#EEFDF7] border-x border-y border-[#69BCA0] text-[#69BCA0] mr-1 mt-1 mb-1"
+                      >
+                        <span className="text-base font-medium font-BaiJamjuree">{tag?.name}</span>
+                      </Badge>
+                    ))}
+                    {project.tags && project.tags.length > 1 && (
+                      <Badge
+                        variant="destructive"
+                        className="h-7 min-w-fit px-[8px] py-[12px] flex items-center justify-center bg-[#EEFDF7] border-x border-y border-[#69BCA0] text-[#69BCA0] mr-1 mt-1 mb-1"
+                      >
+                        <span className="text-base font-medium font-BaiJamjuree">
+                          +{project.tags.length - 1}
+                        </span>
+                      </Badge>
+                    )}</div>
+                    </div>
+
+
+
+               
+
                 <div className="w-[24px] h-[24px] flex flex-row  ">
                   <img src="/asset/icon/budget-black.svg" alt="Budget Icon " />
                   <div className="font-BaiJamjuree text-[14px] font-medium flex text-center">
-                    1,500,000.00
+                    {project.budget}
                   </div>
                 </div>
                 <div className="w-[24px] h-[24px] flex flex-row  ">
                   <img src="/asset/icon/budget-red.svg" alt="Budget Icon " />
                   <div className="font-BaiJamjuree text-[14px] font-medium flex text-center text-[#EF4444]">
-                    1,245,145.14
+                    {project.expense}
+                  </div>
+                </div>
+                <div className="w-[24px] h-[24px] flex flex-row  ">
+                  <img src="/asset/icon/budget-green.svg" alt="Budget Icon " />
+                  <div className="font-BaiJamjuree text-[14px] font-medium flex text-center text-[#69BCA0]">
+                    {project.advance}
                   </div>
                 </div>
                 <div className="flex-row flex">
                   <CrownIcon className="w-[24px] h-[24px] relative text-black" />
                   <TooltipProvider>
                     <div className="flex items-center space-x-[4px]">
-                      {users.map((user) => (
-                        <Tooltip key={user.id}>
+                      {project.owner.map((user) => (
+                        <Tooltip key={user?.id ?? user?.email}>
                           <TooltipTrigger>
                             <div className="flex items-center space-x-2">
                               <div className="w-[24px] h-[24px] bg-gray-100 rounded-full flex items-center justify-center border-[1px] border-brown">
                                 <span className="text-brown text-sm font-BaiJamjuree">
-                                  {getInitials(user.userName)}
+                                  {getInitials(user?.name || "")}
                                 </span>
                               </div>
                             </div>
                           </TooltipTrigger>
                           <TooltipContent>
-                            <p>{user.userName}</p>
+                            <p>{user?.name}</p>
                           </TooltipContent>
                         </Tooltip>
                       ))}
@@ -140,22 +212,22 @@ export const ProjectList_2 = () => {
                   </TooltipProvider>
                 </div>
                 <div className="flex-row flex">
-                  <User className="w-[24px] h-[24px] relative text-black" />
+                  <Users className="w-[24px] h-[24px] relative text-black" />
                   <TooltipProvider>
                     <div className="flex items-center space-x-[4px]">
-                      {users.map((user) => (
-                        <Tooltip key={user.id}>
+                      {project.members.map((user) => (
+                        <Tooltip key={user?.id ?? user?.email}>
                           <TooltipTrigger>
                             <div className="flex items-center space-x-2">
                               <div className="w-[24px] h-[24px] bg-gray-100 rounded-full flex items-center justify-center border-[1px] border-brown">
                                 <span className="text-brown text-sm font-BaiJamjuree">
-                                  {getInitials(user.userName)}
+                                  {getInitials(user?.name || "")}
                                 </span>
                               </div>
                             </div>
                           </TooltipTrigger>
                           <TooltipContent>
-                            <p>{user.userName}</p>
+                            <p>{user?.name}</p>
                           </TooltipContent>
                         </Tooltip>
                       ))}
@@ -165,13 +237,15 @@ export const ProjectList_2 = () => {
 
                 <div className="flex flex-row">
                   <Calendar className="w-[24px] h-[24px] relative text-black" />
-                  {item.startDate && item.endDate && (
+                  {/* {item.startDate && item.endDate && ( */}
                     <div className="text-[14px] font-BaiJamjuree flex  gap-1 items-center">
                       <span>
-                        {formatDate(new Date(project.startDate), new Date(project.endDate))}
+                        {formatDate(
+                          new Date(project.startDate),
+                          new Date(project.endDate)
+                        )}
                       </span>
                     </div>
-                  )}
                 </div>
               </div>
             </div>
