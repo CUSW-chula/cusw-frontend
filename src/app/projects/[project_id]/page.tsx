156acd6bbaac7269d6d4a8415bc3bea2c6acf51f
Mintada P. <mintada.phuangminthada@gmail.com>
2025-02-03 19:47:56 +0700

    fix: fix algor for back button

--- src/app/projects/[project_id]/page.tsx
+++ src/app/projects/[project_id]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TaskManager } from '@/components/elements/taskmanager';
-import { BackButton } from '@/components/elements/backButton';
+import { ProjectBackButton } from '@/components/elements/backButton';
 
 interface TaskManageMentProp {
   params: {
@@ -9,11 +9,11 @@ interface TaskManageMentProp {
 }
 
 export default async function Page({ params }: TaskManageMentProp) {
-  const { project_id } = params;
+  const { project_id } = await params;
   return (
     <div className="min-w-full flex-col items-start justify-center gap-8">
       <div className="flex pb-4 items-center justify-end">
-        <BackButton task_id={project_id} />
+        <ProjectBackButton />
       </div>
       <TaskManager project_id={project_id} />
     </div>
