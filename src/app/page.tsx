badb766b467cec9a3b37763daf90f319080224f0
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-02-16 17:14:11 +0700

    fix: image uploader but it's still not work with .HEIC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 const Home = () => {
   useEffect(() => {
     console.log('Home rendered', process.env.NEXTAUTH_SECRET);
-  }, [])
+  }, []);
   return (
     <div className="fixed inset-0 flex items-center justify-center">
       <Button onClick={() => signIn('google')}>Sign in with Google v2</Button>
