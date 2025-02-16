e075fbf489f5d3fad562d6bd5ce274a593a9bce6
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-02-16 18:10:49 +0700

    fix: useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 import { signIn } from 'next-auth/react';
+import { useEffect } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    console.log("Home ", process.env.NEXTAUTH_SECRET)
+  }, []);
   return (
     <div className="fixed inset-0 flex items-center justify-center">
       <Button onClick={() => signIn('google')}>Sign in with Google v2</Button>
