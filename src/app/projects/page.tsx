b88e2f74baedc4ab61c3c9ef523aed08a3c0aa65
Siwadol_23 <siwadolrungmart@gmail.com>
2025-02-19 21:33:18 +0700

    fix(fix all bug-report 1.#22 pinned project cant use with control bar): fix all bug-report 1.#22

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import { ProjectList } from '@/components/elements/projectList';
+import { ProjectList1 } from '@/components/elements/projectlist2';
 
 export default function ProjectLists() {
   return (
     <div className="flex flex-wrap flex- w-full items-start gap-[16px]  ">
-      <ProjectList />
+      <ProjectList1 />
     </div>
   );
 }
