f94ac3ac0dbd78d1c92d7db4f1160848346d0e9a
Siwadol_23 <siwadolrungmart@gmail.com>
2025-02-19 22:04:58 +0700

    fix(delete projectlist2): delete projectList2

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 import { ProjectList } from '@/components/elements/projectList';
-import { ProjectList1 } from '@/components/elements/projectlist2';
 
 export default function ProjectLists() {
   return (
     <div className="flex flex-wrap flex- w-full items-start gap-[16px]  ">
-      <ProjectList1 />
+      <ProjectList />
     </div>
   );
 }
