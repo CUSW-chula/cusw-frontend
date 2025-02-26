63ccf9e29ae6228f98674094bd3a3e6e36b06aa6
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-02-26 19:16:04 +0700

    fix: url

--- src/app/projects/create/_components/tags.tsx
+++ src/app/projects/create/_components/tags.tsx
@@ -33,7 +33,7 @@ export const ProjectTag = ({ value, onChange }: ProjectTagProps) => {
   React.useEffect(() => {
     const fetchTags = async () => {
       const response = await fetchData(
-        `${BASE_URL}/v2/tags/`,
+        `${BASE_URL}/v2/tags`,
         'GET',
         {},
         'Get Tags From Server Fail.',
