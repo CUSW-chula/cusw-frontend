0c14a0a552b908a6c5a1628bd4934bd6af69f091
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-02-26 19:44:53 +0700

    fix: fix 403

--- src/components/elements/blocknoteproject.tsx
+++ src/components/elements/blocknoteproject.tsx
@@ -96,10 +96,9 @@ function Document({ project_id }: ProjectOverviewProps) {
 
       try {
         const response = await fetch(url, options);
-        if (response.status === 433) {
+        if (response.status === 403) {
           setCanEdit(false);
-        }
-        else if (!response.ok) {
+        } else if (!response.ok) {
           const errorMessage = await response.text();
           // throw new Error("Failed to assign tag");
           toast({
