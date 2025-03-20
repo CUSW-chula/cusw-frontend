1118f3b9e0e0b021d17f208f4eee3e5d5124454e
Phongsakorn0 <pond.phongsakorn1654@gmail.com>
2025-03-21 00:13:47 +0700

    feat(preview): create project template preview

---
+++ src/app/projects/create/_components/taskpreview.tsx
@@ -0,0 +1,40 @@
+import { TaskTitle } from './taskTitle';
+import { useState } from 'react';
+import type { TaskProps } from '@/app/types/types';
+
+export const Task = ({
+  item,
+  depth = 0,
+  hiddenDate,
+}: { item: TaskProps; depth?: number; hiddenDate: boolean }) => {
+  const [expandedTaskIds, setExpandedTaskIds] = useState<Set<string>>(new Set()); //Set of taskID that have been expanded
+  const hasChildren = item.subtasks && item.subtasks.length > 0;
+  return (
+    <>
+      <div className="flex items-center hover:bg-gray-50">
+        <div
+          className="flex items-center w-full h-fit my-1.5"
+          style={{ marginLeft: `${depth * 24 + 24}px` }}>
+          <TaskTitle
+            item={item}
+            expandedTaskIds={expandedTaskIds}
+            setExpandedTaskIds={setExpandedTaskIds}
+          />
+        </div>
+      </div>
+
+      {hasChildren && expandedTaskIds.has(item.id) && (
+        <div>
+          {item.subtasks?.map((child) => (
+            <Task
+              key={child.id}
+              item={child}
+              depth={depth + 1} // Increase depth for child tasks
+              hiddenDate={hiddenDate}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
