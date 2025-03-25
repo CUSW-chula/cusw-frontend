445edcceb139d31f9922403b9157b210c32e6176
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-03-26 01:29:36 +0700

    fix: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import type { Metadata } from 'next';
 import { Bai_Jamjuree, Anuphan } from 'next/font/google';
 import './globals.css';
 import NavBar from '@/components/elements/nav-bar';
 import { SessionProvider } from 'next-auth/react';
-import { Toast } from '@radix-ui/react-toast';
 import { Toaster } from '@/components/ui/toaster';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
 
 const bai_jamjuree = Bai_Jamjuree({
   subsets: ['latin'],
@@ -27,16 +27,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const url = usePathname();
+
+  // Allowed routes where NavBar should be displayed
+  const allowedRoutes = ['/my-task', '/user-management', '/projects'];
+
+  // Use useMemo to avoid unnecessary recalculations on fast refresh
+  const isAllowed = useMemo(() => {
+    return allowedRoutes.includes(url) || url.startsWith('/projects/') || url.startsWith('/tasks/');
+  }, [url]);
+
   return (
     <html lang="en">
       <SessionProvider>
         <body
           className={`min-h-lvh ${bai_jamjuree.variable} ${anuphan.variable} antialiased flex flex-col`}>
-          <div className="flex flex-row justify-between">
-            <NavBar />
-          </div>
+          <div className="flex flex-row justify-between">{isAllowed && <NavBar />}</div>
 
-          <div className="w-full px-20 flex justify-center">{children}</div>
+          <div className="w-full flex justify-center">{children}</div>
           <Toaster />
         </body>
       </SessionProvider>
