8edfd9f6b58e2dc34a3e2472ad645f87a8d17e3e
bunyawatapp37204 <stonksfarm37204@gmail.com>
2025-03-26 21:07:57 +0700

    fix: styling code

--- src/components/elements/blocknote.tsx
+++ src/components/elements/blocknote.tsx
@@ -140,7 +140,7 @@ function Document({ description }: Description) {
     collaboration: {
       provider,
       fragment: doc.getXmlFragment('blocknote'),
-      user: { color: getRandomLightColor(), name: userName},
+      user: { color: getRandomLightColor(), name: userName },
       // showCursorLabels: 'always',
     },
   });
