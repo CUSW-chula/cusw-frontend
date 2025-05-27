e80b292a4f58cee22b3860f944b9cbc3c98314cb
Nathawat Chan-iat <nathawat12246@gmail.com>
2025-05-27 17:55:10 +0700

    fix: add sort after get tasks

--- src/app/types/types.tsx
+++ src/app/types/types.tsx
@@ -11,6 +11,7 @@ export interface TaskProps {
   status: 'Unassigned' | 'Assigned' | 'UnderReview' | 'InRecheck' | 'Done';
   projectId: string;
   parentTaskId: string; //fix to type initial,subtaskAdded,...
+  position?: string; 
   statusBudget: string; //Name not mathch the db 'Budgets / Budget'
   budget: number;
   advance: number;
