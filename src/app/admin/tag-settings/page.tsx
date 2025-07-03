e2ae85fb3f283d6f4fbc61e683d34f845c661715
bunyawat.n <bunyawatnaunnak@gmail.com>
2025-07-03 18:42:41 +0700

    feat: back button

--- src/app/admin/tag-settings/page.tsx
+++ src/app/admin/tag-settings/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 import React from 'react';
 import Table from '@/components/elements/tagManagement/table';
+import { AdminBackButton } from '@/components/elements/backButton';
 const page = () => {
   return (
     <div className="w-full px-20">
+      <div className="flex w-full items-center justify-end mb-4">
+        <AdminBackButton />
+      </div>
       <Table />
     </div>
   );
