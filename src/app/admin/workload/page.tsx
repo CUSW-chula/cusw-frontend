b967991043290590220054c7e97cb1b0ab8e810f
Siwadol_23 <167745512+siwadolrungmart@users.noreply.github.com>
2025-07-05 20:27:29 +0700

    Update page.tsx

--- src/app/admin/workload/page.tsx
+++ src/app/admin/workload/page.tsx
@@ -4,7 +4,7 @@ import { AdminBackButton } from '@/components/elements/backButton';
 import React from 'react';
 const page = () => {
   return (
-    <div className="min-w-full flex-col items-start justify-center  p-20 ">
+    <div className="min-w-full flex-col items-start justify-center  px-20 pb-20">
       <WorkloadUser />
     </div>
   );
