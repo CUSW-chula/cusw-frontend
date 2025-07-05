422f2bc8fba0b2ab6f1f11fdfbd63c5fa01848b4
Siwadol_23 <167745512+siwadolrungmart@users.noreply.github.com>
2025-07-05 21:50:53 +0700

    Update chart-table-workload.tsx

--- src/components/elements/workload/AllUsers/chart-table-workload.tsx
+++ src/components/elements/workload/AllUsers/chart-table-workload.tsx
@@ -60,7 +60,8 @@ export function WorkloadChart({ userWorkload }: UserWorkloadProps) {
     <>
       <CardContentCustom>
         <div className="w-full overflow-x-auto bg-white py-[30px] px-[15px] rounded-md  outline outline-1 outline-stone-600">
-          <div style={{ minWidth: `${chartWidth}px`, height: '500px' }}>
+          <div  className="relative h-[500px]"
+  style={{ minWidth: `${chartWidth}px` }}>
             <ChartContainer config={chartConfig} className="max-h-[500px] w-full">
               <BarChart
                 width={chartWidth}
