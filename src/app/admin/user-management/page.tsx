3839fab222d7c9ca76952432b7565d32aba3e1b0
Bunyawat Naunnak <70959980+bunnybunbun37204@users.noreply.github.com>
2025-07-06 20:22:31 +0700

    Update page.tsx

--- src/app/admin/user-management/page.tsx
+++ src/app/admin/user-management/page.tsx
@@ -4,7 +4,7 @@ import { AdminBackButton } from '@/components/elements/backButton';
 const page = () => {
   return (
     <div className="min-w-full flex-col items-start justify-center gap-8 px-20">
-      <div className="flex w-full items-center justify-end">
+      <div className="flex w-full items-center justify-end mb-4">
         <AdminBackButton />
       </div>
       <Table />
