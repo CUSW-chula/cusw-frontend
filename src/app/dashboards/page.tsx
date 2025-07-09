f134e991683bab27d35753280127e97569afb5f2
Siwadol_23 <siwadolrungmart@gmail.com>
2025-07-09 19:14:34 +0700

    fix(fix dashboards): fix dashboards

--- src/app/dashboards/page.tsx
+++ src/app/dashboards/page.tsx
@@ -41,7 +41,7 @@ const page = () => {
       <h1 className="text-5xl font-bold text-brown mb-4 font-BaiJamjuree w-full">Dashboard</h1>
       <div className="grid w-full justify-center items-center grid-cols-[repeat(auto-fit,_minmax(200px,_310px))] gap-8">
         <DashboardCard title="Workload Distribution" path="/workload" />
-        <GanttChartCard title="Gant Chart" path="/dashboard/project" />
+        <GanttChartCard title="Gantt Chart" path="/dashboard/project" />
       </div>
     </div>
   );
