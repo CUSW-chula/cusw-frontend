23fc8608ad7c4503dc597ca24acc76e41a70f566
Siwadol_23 <siwadolrungmart@gmail.com>
2025-09-12 00:12:38 +0700

    fix(fix pls fix): fix pls fix

--- src/components/elements/assigned-task.tsx
+++ src/components/elements/assigned-task.tsx
@@ -36,7 +36,7 @@ export function AssignedTaskToMember({ task }: { task: TaskProps }) {
   const [usersList, setUsersList] = React.useState<UsersInterfaces[]>([]);
   const [owner, setOwner] = React.useState<UsersInterfaces[]>([]);
   const MAX_VISIBLE_MEMBERS = 3;
-  const [ownerIds, setOwnerIds] = React.useState<string[]>([]);
+ 
 
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   const pareJsonValue = React.useCallback((values: any) => {
@@ -77,7 +77,7 @@ export function AssignedTaskToMember({ task }: { task: TaskProps }) {
           // throw new Error("Failed to assign tag");
         }
         const data = await response.json();
-        setOwnerIds(data.owner?.map((o: UsersInterfaces) => o.id) || []);
+       
       } catch (error) {
         console.error('Error fetching Owner:', error);
       }
@@ -202,7 +202,7 @@ export function AssignedTaskToMember({ task }: { task: TaskProps }) {
                   <CommandEmpty>No results found.</CommandEmpty>
                   <CommandGroup>
                     {usersList
-                      .filter((user) => !ownerIds.includes(user.id))
+                      
                       .map((user) => (
                         <CommandItem key={user.id} value={user.name} onSelect={handleSelectUser}>
                           <Circle
