779f578609883f68308200fce1727f6fe61fede5
Bunyawat Naunnak <bunyawatnaunnak@My-Mac.local>
2025-10-17 23:24:02 +0700

    fix: use client for test

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import { ProjectList } from '@/components/elements/projectList/project-list';
 import BASE_URL, { BASE_SOCKET, BASE_YSWEET } from '@/lib/shared';
 import React from 'react';
 
-export default async function ProjectLists() {
-  console.log('🔍 [Projects Page] Environment Variables:', {
+export default function ProjectLists() {
+  // Log ฝั่ง client (จะเห็นใน Browser Console)
+  console.log('🔍 [Projects Page - CLIENT] Environment Variables:', {
     NEXT_PUBLIC_IS_DEV: process.env.NEXT_PUBLIC_IS_DEV,
     NODE_ENV: process.env.NODE_ENV,
     BASE_URL,
